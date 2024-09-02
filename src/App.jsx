9cbf20dad746f59b29678106e2745d2156670c11
Tshegofatso Mogotlane <tshegofatsom177@gmail.com>
2024-09-02 18:56:02 +0200

    Initial Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to The Future</h1>
+      <p>Start building your amazing app!</p>
+    </div>
+  );
+}
+
+export default App;
